8aa5e83b6da6f2c994f52a0add6426acc23006eb
MerelPennekamp <m.pennekamp@student.tue.nl>
2020-04-27 11:26:34 +0200

    Load the corona world and NL datasets into R

---
+++ corona_datasets.R
@@ -0,0 +1,7 @@
+#Read the corona world dataset from csv file
+corona_world <- read.csv("~/GitHub/2img10-group-6/corona_world")
+View(corona_world)
+
+#Read the corona Netherlands dataset from excel file
+corona_nl <- read_excel("COVIDNL.xlsx")
+View(corona_nl)
